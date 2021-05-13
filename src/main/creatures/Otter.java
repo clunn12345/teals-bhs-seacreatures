edbf125101940035e7ecb3dc9af35ec2cb55ea60
clunn12345 <clunn12345@gmail.com>
2021-05-13 09:12:39 -0400

    made otter class

---
+++ src/main/creatures/Otter.java
@@ -0,0 +1,8 @@
+package creatures;
+
+public class Otter extends AquaticMammal {
+    public String toString(){return "beach dweller";}
+
+    public void method2(){System.out.println("paws");}
+
+}
